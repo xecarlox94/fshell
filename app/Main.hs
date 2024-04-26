ff7ff1de68b6b752ca6aa6e8f5f57ab47431557d
xecarlox94 <jf94.uk@gmail.com>
2024-04-26 08:38:04 +0100

    working with megaparsec

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import qualified Prelude as P
+
+--import              Control.Monad
+import              Control.Applicative hiding (many)
+
+import              Data.Text (Text)
+import              Data.Void
+
+import              Text.Megaparsec (Parsec)
+
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+-- main = forever $ do
+--     putStr "> "
+--     a <- getLine
+--     putStr $ a ++ "\n\n"
+
+main :: P.IO ()
+main = do P.putStrLn "SIUUU"
+
+-- https://markkarpov.com/tutorial/megaparsec.html
+
+type Parser = Parsec Void Text
+
+
+mySequence :: Parser (P.Char, P.Char, P.Char)
+mySequence = do
+    (,,)    <$> char 'a'
+            <*> char 'b'
+            <*> char 'c'
+
+
